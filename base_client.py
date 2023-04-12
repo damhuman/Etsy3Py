f125baee55cdf0081175a4354fcff1fb84f816f1
Anton <anton.dasyuk@gmail.com>
2023-04-12 14:35:51 +0300

    Create base_client.py

---
+++ base_client.py
@@ -0,0 +1,48 @@
+from requests.auth import HTTPBasicAuth
+from requests import Request, Session
+ 
+ 
+class BaseApiClient:
+    base_url = None  # Override me
+    __token_type = None
+    __token = None
+    __token_expire = None
+    session = Session()
+ 
+    def _make_request(self,
+                      path: str,
+                      custom_base: str = None,
+                      method: str = 'GET',
+                      headers: dict = None,
+                      data: dict = None,
+                      params: dict = None,
+                      auth_type: str = 'token'):
+        if auth_type == 'token':
+            self.check_token()
+        if not headers:
+            headers = {}
+ 
+        request_url = f"{custom_base if custom_base else self.base_url}{path}"
+ 
+        auth = None
+        if auth_type == 'basic':
+            auth = HTTPBasicAuth(1, 1)
+        if auth_type == 'token':
+            headers['Authorization'] = f'{self.__token_type} {self.__token}'
+ 
+        request = Request(method=method,
+                          url=request_url,
+                          headers=headers,
+                          data=data,
+                          params=params,
+                          auth=auth).prepare()
+        response = self.session.send(request)
+        return response
+ 
+    def _post(self, path: str, data: dict = None, headers: dict = None, auth_type: str = 'none'):
+        return self._make_request(path=path, method='POST', data=data,
+                                  headers=headers, auth_type=auth_type)
+ 
+    def _get(self, path: str, params: dict = None, headers: dict = None, auth_type: str = 'none'):
+        return self._make_request(path=path, method='GET', params=params,
+                                  headers=headers, auth_type=auth_type)

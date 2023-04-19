28aaed15573010b74ca38680cfb5164b0bf53276
ali <sashaal-sayed@macbook-pro-sasha.local>
2023-04-19 20:22:13 +0300

    Update etsyapi

--- src/etsy3py/base_client.py
+++ src/etsy3py/base_client.py
@@ -11,7 +11,7 @@ class BaseApiClient:
     __token = None
     __client_id = None
     session = Session()
- 
+
     def _make_request(self,
                       path: str,
                       custom_base: str = None,
@@ -23,16 +23,16 @@ class BaseApiClient:
 
         if not headers:
             headers = {}
- 
+
         request_url = f"{custom_base if custom_base else self.base_url}{path}"
- 
+
         auth = None
         if auth_type == 'basic':
             auth = HTTPBasicAuth(1, 1)
         if auth_type == 'token':
             headers['Authorization'] = f'{self.__token_type} {self.__token}'
             headers['x-api-key'] = f'{self.__client_id}'
- 
+
         request = Request(method=method,
                           url=request_url,
                           headers=headers,
@@ -41,15 +41,20 @@ class BaseApiClient:
                           auth=auth).prepare()
         response = self.session.send(request)
         return response
- 
+
     def _post(self, path: str, data: dict = None, headers: dict = None,
               auth_type: str = 'none') -> requests.Response:
         return self._make_request(path=path, method='POST', data=data,
                                   headers=headers, auth_type=auth_type)
- 
+
     def _get(self, path: str, params: dict = None, headers: dict = None,
              auth_type: str = 'none') -> requests.Response:
         return self._make_request(path=path, method='GET', params=params,
+                                  headers=headers, auth_type=auth_type)
+
+    def _patch(self, path: str, data: dict = None, headers: dict = None,
+               auth_type: str = 'none') -> requests.Response:
+        return self._make_request(path=path, method='PATCH', data=data,
                                   headers=headers, auth_type=auth_type)
 
     def _put(self, path: str, data: dict = None, headers: dict = None,
